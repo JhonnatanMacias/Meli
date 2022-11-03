01381eb52c1ed8d285ab58c55791f2c5e7f34532
Jhonnatan <jhonnatan.macias@walmart.com>
2022-11-02 21:38:13 -0500

    Setup and initialization

---
+++ Meli/Meli/Services/MeliServices.swift
@@ -0,0 +1,52 @@
+//
+//  MeliServices.swift
+//  Meli
+//
+//  Created by Jhonnatan Macias on 31/10/22.
+//
+
+import Alamofire
+import Foundation
+
+class RecipesServices: BaseService {
+
+    struct Constants {
+        static let endPoint = "recipes/random?number=30"
+        static let baseURL = "https://api.spoonacular.com/"
+        static let searchEndPoint = "recipes/complexSearch?query="
+    }
+
+    init() {
+        super.init(baseURL: Constants.baseURL)
+    }
+
+    /*func getRamdomRecipes(completionHandler: @escaping(Result<Recipes, Error>) -> Void) {
+
+        getCodable(URL: baseURL + Constants.endPoint,
+                   parameters: nil, responseType: Recipes.self) { (response: Recipes?, error: NSError?) in
+            if let error = error {
+                completionHandler(.failure(error as Error))
+            } else if let response = response {
+                completionHandler(.success(response))
+            } else {
+                let erro: Error =  NSError(domain: "", code: 404, userInfo: nil) as Error
+                completionHandler(.failure(erro))
+            }
+        }
+    }
+
+    func getRecipes(title: String, completionHandler: @escaping(Result<SearchRecipe, Error>) -> Void) {
+        let api = baseURL + Constants.searchEndPoint + title + "&number=30"
+        getCodable(URL: api,
+                   parameters: nil, responseType: SearchRecipe.self) { (response: SearchRecipe?, error: NSError?) in
+            if let error = error {
+                completionHandler(.failure(error as Error))
+            } else if let response = response {
+                completionHandler(.success(response))
+            } else {
+                let erro: Error =  NSError(domain: "", code: 404, userInfo: nil) as Error
+                completionHandler(.failure(erro))
+            }
+        }
+    }*/
+}
---
+++ Meli/Meli/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meli
+//
+//  Created by Jhonnatan Macias on 31/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
